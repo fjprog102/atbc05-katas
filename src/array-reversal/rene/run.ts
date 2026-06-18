8028f30e22449aee97cf9492e8f67d3ce247a726
rene-ad <rene.diaz@assuresoftbolivia.com>
2026-06-18 10:48:49 -0400

    Improve run.ts files from sum-digits/rene and array-reversal/rene folders

--- src/array-reversal/rene/run.ts
+++ src/array-reversal/rene/run.ts
@@ -1,20 +1,28 @@
 import { ArrayRotation } from './array-reversal';
 
-console.log(ArrayRotation.bruteForce([1, 2, 3, 4, 5], 2));
-console.log(ArrayRotation.bruteForce([1, 2, 3, 4, 5], 7));
-console.log(ArrayRotation.bruteForce([1, 2, 3, 4, 5], -1));
-console.log(ArrayRotation.bruteForce([1, 2, 3, 4, 5], -7));
-console.log(ArrayRotation.bruteForce([1, 2, 3, 4, 5], 0));
+const testArray = [1, 2, 3, 4, 5];
+console.log('Brute Force:');
+console.log('K = 2: ' + ArrayRotation.bruteForce(testArray, 2));
+console.log('K = 7: ' + ArrayRotation.bruteForce(testArray, 7));
+console.log('K = -1: ' + ArrayRotation.bruteForce(testArray, -1));
+console.log('K = -7: ' + ArrayRotation.bruteForce(testArray, -7));
+console.log('K = 0: ' + ArrayRotation.bruteForce(testArray, 0));
+console.log('Empty array: ' + ArrayRotation.bruteForce([], 5));
+console.log('Array with strings: ' + ArrayRotation.bruteForce(['a', 'b', 'c'], 5));
+console.log('Array with one element: ' + ArrayRotation.bruteForce([100], 5));
 
-console.log('---');
-console.log(ArrayRotation.reversalMethod([1, 2, 3, 4, 5], 2));
-console.log(ArrayRotation.reversalMethod([1, 2, 3, 4, 5], 7));
-console.log(ArrayRotation.reversalMethod([1, 2, 3, 4, 5], -1));
-console.log(ArrayRotation.reversalMethod([1, 2, 3, 4, 5], -7));
-console.log(ArrayRotation.reversalMethod([1, 2, 3, 4, 5], 0));
+console.log('Reversal Method:');
+console.log('K = 2: ' + ArrayRotation.reversalMethod(testArray, 2));
+console.log('K = 7: ' + ArrayRotation.reversalMethod(testArray, 7));
+console.log('K = -1: ' + ArrayRotation.reversalMethod(testArray, -1));
+console.log('K = -7: ' + ArrayRotation.reversalMethod(testArray, -7));
+console.log('K = 0: ' + ArrayRotation.reversalMethod(testArray, 0));
+console.log('Empty array: ' + ArrayRotation.reversalMethod([], 5));
+console.log('Array with strings: ' + ArrayRotation.reversalMethod(['a', 'b', 'c'], 5));
+console.log('Array with one element: ' + ArrayRotation.reversalMethod([100], 5));
 
+console.log('Comparison with large arrays:');
 const largeArray = [];
-
 for (let i = 1; i <= 100000; i++) {
     largeArray.push(i);
 }
--- src/sum-digits/rene/run.ts
+++ src/sum-digits/rene/run.ts
@@ -1,21 +1,23 @@
 import { DigitRoot } from './sum-digits';
 
-console.log(DigitRoot.iterative(38));
-console.log(DigitRoot.iterative(9875));
-console.log(DigitRoot.iterative(9));
-console.log(DigitRoot.iterative(0));
-console.log(DigitRoot.iterative(-38));
+console.log('Iterative method:');
+console.log('38: ' + DigitRoot.iterative(38));
+console.log('9875: ' + DigitRoot.iterative(9875));
+console.log('9: ' + DigitRoot.iterative(9));
+console.log('0: ' + DigitRoot.iterative(0));
+console.log('-38: ' + DigitRoot.iterative(-38));
+
+console.log('Mathematical method:');
+console.log('38: ' + DigitRoot.mathematical(38));
+console.log('9875: ' + DigitRoot.mathematical(9875));
+console.log('9: ' + DigitRoot.mathematical(9));
+console.log('0: ' + DigitRoot.mathematical(0));
+console.log('-38: ' + DigitRoot.mathematical(-38));
 
 console.log('---');
-console.log(DigitRoot.mathematical(38));
-console.log(DigitRoot.mathematical(9875));
-console.log(DigitRoot.mathematical(9));
-console.log(DigitRoot.mathematical(0));
-console.log(DigitRoot.mathematical(-38));
-
-console.log('---');
-console.log(DigitRoot.iterativeWithSteps(38));
-console.log(DigitRoot.iterativeWithSteps(9875));
-console.log(DigitRoot.iterativeWithSteps(9));
-console.log(DigitRoot.iterativeWithSteps(0));
-console.log(DigitRoot.iterativeWithSteps(-38));
+console.log('Iterative method with steps:');
+console.log('38: ', DigitRoot.iterativeWithSteps(38));
+console.log('9875: ', DigitRoot.iterativeWithSteps(9875));
+console.log('9: ', DigitRoot.iterativeWithSteps(9));
+console.log('0: ', DigitRoot.iterativeWithSteps(0));
+console.log('-38: ', DigitRoot.iterativeWithSteps(-38));
