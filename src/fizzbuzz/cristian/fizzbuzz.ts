de1aaf26f9ba730fb4b32018a5769bf6d21da031
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-06-18 00:03:08 -0400

    Implement interface and function

---
+++ src/fizzbuzz/cristian/fizzbuzz.ts
@@ -0,0 +1,11 @@
+
+interface FizzBuzzRule{
+    divisor: number;
+    word: string;
+}
+
+export function fizBuzzExtended(n: number, rules: FizzBuzzRule[]): string[]{
+    const result: string[] = [];
+
+    return result
+}
