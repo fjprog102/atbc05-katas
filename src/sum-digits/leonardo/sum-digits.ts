878b65fdbd818df0c52c3b6f4c079209ff089ba1
Leonardo Eguino <leonardoeguinorv@gmail.com>
2026-06-18 10:23:06 -0400

    feat: implemented class signature

---
+++ src/sum-digits/leonardo/sum-digits.ts
@@ -0,0 +1,13 @@
+export class DigitRoot {
+    static iterative(num: number): number {
+        throw Error('Not implemented')
+    }
+
+    static mathematical(num: number): number {
+        throw Error('Not implemented')
+    }
+
+    static iterativeWithSteps(num: number): {result: number, steps: number} {
+        throw Error('Not implemented')
+    }
+}
