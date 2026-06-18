b354e3156bb7fb0c6eff1b3f4333c26a33631d59
CrisCanapi730 <cristiancanapi730@gmail.com>
2026-06-18 00:17:29 -0400

    Function fizzbuzz extended implemented

--- src/fizzbuzz/cristian/fizzbuzz.ts
+++ src/fizzbuzz/cristian/fizzbuzz.ts
@@ -6,6 +6,26 @@ interface FizzBuzzRule{
 
 export function fizBuzzExtended(n: number, rules: FizzBuzzRule[]): string[]{
     const result: string[] = [];
+    
+    for (let i = 0; i<=n; i++)
+    {
+        let word = "";
+
+        for(const rule of rules)
+        {
+            if(i%rule.divisor===0){
+                word = word + rule.word;
+            }
+        }
+
+        if(word !== ""){
+            result.push(word)
+        }
+        else
+        {
+            result.push(String(i));
+        }
+    }
 
     return result
 }
