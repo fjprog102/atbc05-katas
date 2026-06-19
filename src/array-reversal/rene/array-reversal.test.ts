146480960d6271360cd9e2513919ff50ee05e227
Rene Diaz <ddcrene@gmail.com>
2026-06-19 09:11:18 -0400

    Add 3rd unit test for brute force method with steps in array-reversal/rene/array-reversal.test.ts

--- src/array-reversal/rene/array-reversal.test.ts
+++ src/array-reversal/rene/array-reversal.test.ts
@@ -15,4 +15,11 @@ describe('Brute force method for array reversal', () => {
         const actualResult = ArrayRotation.bruteForce(testArray, k);
         expect(actualResult).toEqual(expectedResult);
     });
+    it('3. Should return the array rotated two positions to the left when array length is 5 and k=-2', () => {
+        const testArray = [1, 2, 3, 4, 5];
+        const k = -2;
+        const expectedResult = [3, 4, 5, 1, 2];
+        const actualResult = ArrayRotation.bruteForce(testArray, k);
+        expect(actualResult).toEqual(expectedResult);
+    });
 });
