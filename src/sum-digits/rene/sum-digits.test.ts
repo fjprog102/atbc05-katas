99adab171927a5265d69bcee6eaa523edf4a121c
Rene Diaz <ddcrene@gmail.com>
2026-06-19 08:21:31 -0400

    Add 2nd unit test for iterative method in sum-digits/rene/sum-digits.test.ts

--- src/sum-digits/rene/sum-digits.test.ts
+++ src/sum-digits/rene/sum-digits.test.ts
@@ -1,10 +1,16 @@
 import { DigitRoot } from './sum-digits';
 
 describe('Iterative method for sum-digits', () => {
-  it('Should return the sum of digits for a positive number', () => {
-    const number = 38;
-    const expectedResult = 2;
-    const actualResult = DigitRoot.iterative(number);
-    expect(actualResult).toBe(expectedResult);
-  });
+    it('Should return the sum of digits for a positive number', () => {
+        const number = 38;
+        const expectedResult = 2;
+        const actualResult = DigitRoot.iterative(number);
+        expect(actualResult).toBe(expectedResult);
+    });
+    it('Should return the sum of digits for a negative number', () => {
+        const number = -38;
+        const expectedResult = 2;
+        const actualResult = DigitRoot.iterative(number);
+        expect(actualResult).toBe(expectedResult);
+    });
 });
