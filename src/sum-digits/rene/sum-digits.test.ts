7e163d24d20fda2e1c50a5c339977d6197a943f0
Rene Diaz <ddcrene@gmail.com>
2026-06-19 08:28:39 -0400

    Refactor unit test names from sum-digits/rene/sum-digits.test.ts

--- src/sum-digits/rene/sum-digits.test.ts
+++ src/sum-digits/rene/sum-digits.test.ts
@@ -1,25 +1,25 @@
 import { DigitRoot } from './sum-digits';
 
 describe('Iterative method for sum-digits', () => {
-    it('Should return the sum of digits for a positive number', () => {
+    it('1. Should return the sum of digits for a positive number with 2 iterations', () => {
         const number = 38;
         const expectedResult = 2;
         const actualResult = DigitRoot.iterative(number);
         expect(actualResult).toBe(expectedResult);
     });
-    it('Should return the sum of digits for a negative number', () => {
+    it('2. Should return the sum of digits for a negative number with 2 iterations', () => {
         const number = -38;
         const expectedResult = 2;
         const actualResult = DigitRoot.iterative(number);
         expect(actualResult).toBe(expectedResult);
     });
-    it('Should return the sum of digits for a positive number with only one digit', () => {
+    it('3. Should return the sum of digits for a positive number with only one digit', () => {
         const number = 5;
         const expectedResult = 5;
         const actualResult = DigitRoot.iterative(number);
         expect(actualResult).toBe(expectedResult);
     });
-    it('Should return the sum of digits for 0', () => {
+    it('4. Should return the sum of digits for 0', () => {
         const number = 0;
         const expectedResult = 0;
         const actualResult = DigitRoot.iterative(number);
