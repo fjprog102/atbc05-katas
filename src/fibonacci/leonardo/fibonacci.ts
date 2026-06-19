ebe14ca32716bd938e951556040d9d57aad20054
Leonardo Eguino <leonardoeguinorv@gmail.com>
2026-06-19 16:07:39 -0400

    feat: implemented getNthIterative method

--- src/fibonacci/leonardo/fibonacci.ts
+++ src/fibonacci/leonardo/fibonacci.ts
@@ -1,10 +1,23 @@
 export class FibonacciGenerator {
+    private _startA: number;
+    private _startB: number;
+
     constructor(startA: number, startB: number) {
-        throw Error('Not implemented');
+        this._startA = startA;
+        this._startB = startB;
     }
 
     getNthIterative(n: number): number {
-        throw Error('Not implemented');
+        if(n === 0) return this._startA;
+        if(n === 1) return this._startB;
+
+        for(let i=1; i<n; i++){
+            const aux = this._startA + this._startB;
+            this._startA = this._startB;
+            this._startB = aux;
+        }
+
+        return this._startB;
     }
 
     getNthRecursive(n: number): number {
