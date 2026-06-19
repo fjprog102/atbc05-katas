e4a236929cf5c6674894761adda141fbfc771ad9
Leonardo Eguino <leonardoeguinorv@gmail.com>
2026-06-19 11:34:39 -0400

    feat: Defined FibonacciGenerator class without implementation

---
+++ src/fibonacci/leonardo/fibonacci.ts
@@ -0,0 +1,17 @@
+export class FibonacciGenerator {
+    constructor(startA: number, startB: number) {
+        throw Error('Not implemented');
+    }
+
+    getNthIterative(n: number): number {
+        throw Error('Not implemented');
+    }
+
+    getNthRecursive(n: number): number {
+        throw Error('Not implemented');
+    }
+
+    clearCache(): void {
+        throw Error('Not implemented')
+    }
+}
